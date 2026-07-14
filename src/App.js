9e6976c49b67a08df63ae57ff8ba7f2e9f0677a2
stkarthik <mailstkarthik@gmail.com>
2026-07-14 18:38:24 +0530

    changes for sqare and board

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
+import {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function Square() {
+
+function Square(){
+  const [value,setValue] = useState(null);
+
+  function handleClick(){
+    setValue('X');
+  }
+
   return (
-    <button className="square">X</button>
+    <>
+      <button className="square" onClick={handleClick}>{value}</button>
+    </>
+  )
+}
+
+function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
   );
 }
 
-export default Square;
+export default Board;
